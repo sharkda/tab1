50a7dc78e5e0ad238821a04fb372bfead5c6f38c
Sharkda <jimhsuyc@gmail.com>
2019-05-17 10:07:04 +0800

    first commit

---
+++ tab1/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tab1
+//
+//  Created by Jim Hsu on 2019/5/17.
+//  Copyright © 2019 shark.com.tw. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
